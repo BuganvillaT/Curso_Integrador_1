70059fac30ab10e8310b3d013cd47b5ca2f99586
BungavillaT <ingenierodesistemaseinformacio@gmail.com>
2025-10-26 12:37:07 -0500

    archivo pom.xml modificado

---
+++ buganvilla-tours-backend/src/main/java/com/buganvillatours/BuganvillaToursBackendApplication.java
@@ -0,0 +1,13 @@
+package com.buganvillatours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuganvillaToursBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuganvillaToursBackendApplication.class, args);
+	}
+
+}
