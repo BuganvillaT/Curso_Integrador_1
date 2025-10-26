90c8b00f0af389d39750649ec08b402bbc0350f4
BungavillaT <ingenierodesistemaseinformacio@gmail.com>
2025-10-26 13:18:37 -0500

    com/BuganvillaToursBackendApplication.java modificado

--- buganvilla-tours-backend/src/main/java/com/buganvillatours/BuganvillaToursBackendApplication.java
+++ buganvilla-tours-backend/src/main/java/com/buganvillatours/BuganvillaToursBackendApplication.java
@@ -2,12 +2,9 @@ package com.buganvillatours;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class BuganvillaToursBackendApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BuganvillaToursBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BuganvillaToursBackendApplication.class, args);
+    }
 }
