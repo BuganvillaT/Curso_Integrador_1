1df3225fd17a74d4dc81cbb21475ea301f996b6c
BungavillaT <ingenierodesistemaseinformacio@gmail.com>
2025-10-28 18:04:52 -0500

    Agregando usuario y contraseña al spring.security

--- buganvilla-tours-backend/buganvilla-tours-backend/src/main/java/dao/ReservaDAO.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import com.buganvillatours.entity.Reserva;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ReservaDAO extends JpaRepository<Reserva, Integer> {
-    List<Reserva> findByUsuarioIdUsuario(int idUsuario); // Ejemplo de consulta personalizada
-}
